a2243670cb3b88b89acd7fea23587b1544dd6d1f
Samuel Macleod <samuel@macleod.space>
2018-07-25 00:41:42 +0100

    Added twiiter component back in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class ApplicationController < ActionController::Base
     # Give them the name of a model (like @teams instead of @global_teams) and
     # ActiveAdmin will break in marvelous ways, as it includes this controller
     @upcoming_events = Event.upcoming
-    # Removed until Tardis can fix the firewall issue
-    # @tweets = TwitterService.get_timeline
+    @tweets = TwitterService.get_timeline
     @global_pages = Page.all.order(priority: :desc)
     @global_teams = Team.order(:display_order)
   end
