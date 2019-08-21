53c5744f8c16cfaf1293b8c31189810f6fd56bc8
macleos <samuel@macleod.space>
2019-08-21 03:44:56 +0100

    Add audio to accepted tags

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -45,7 +45,7 @@ module ContentHelper
   def accepted_tags
     %w(strong em b i p code pre tt samp kbd var sub
     sup dfn cite big small address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dl dt dd abbr
-    acronym a img blockquote del ins iframe table tr th td)
+    acronym a img blockquote del ins iframe table tr th td audio)
   end
 
   def accepted_attributes
